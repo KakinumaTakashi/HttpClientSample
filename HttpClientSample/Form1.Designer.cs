663153a94ffa95b3419befdf04eef140b4334fd6
Takashi Kakinuma <kakinuma.takashi@homes.ecweb.jp>
2020-04-09 17:01:01 +0900

    ダウンロード処理を実装。

---
+++ HttpClientSample/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace HttpClientSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonDownload = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ButtonUpload = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ButtonDownload
+            // 
+            this.ButtonDownload.Location = new System.Drawing.Point(3, 3);
+            this.ButtonDownload.Name = "ButtonDownload";
+            this.ButtonDownload.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDownload.TabIndex = 0;
+            this.ButtonDownload.Text = "Download";
+            this.ButtonDownload.UseVisualStyleBackColor = true;
+            this.ButtonDownload.Click += new System.EventHandler(this.ButtonDownload_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.ButtonUpload);
+            this.panel1.Controls.Add(this.ButtonDownload);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1003, 35);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(184, 165);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBox);
+            this.panel2.Location = new System.Drawing.Point(12, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1003, 509);
+            this.panel2.TabIndex = 3;
+            // 
+            // ButtonUpload
+            // 
+            this.ButtonUpload.Location = new System.Drawing.Point(85, 4);
+            this.ButtonUpload.Name = "ButtonUpload";
+            this.ButtonUpload.Size = new System.Drawing.Size(75, 23);
+            this.ButtonUpload.TabIndex = 1;
+            this.ButtonUpload.Text = "Upload";
+            this.ButtonUpload.UseVisualStyleBackColor = true;
+            this.ButtonUpload.Click += new System.EventHandler(this.ButtonUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 575);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonDownload;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button ButtonUpload;
+    }
+}
+
---
+++ HttpClientSample/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+using Windows.Data.Pdf;
+
+namespace HttpClientSample
+{
+    /// <summary>
+    /// HTTP通信・PDF変換サンプル
+    /// <see cref="https://qiita.com/rawr/items/f78a3830d894042f891b"/>
+    /// <see cref="https://water2litter.net/rum/post/cs_pdf_wpf/"/>
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        // Nifty Mobile Backend - File store
+        private const string REST_API_URL = @"https://mbaas.api.nifcloud.com/2013-09-01/applications/KFZmOFF9ZXWbdfQS/publicFiles/test.pdf";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// MBAASからPDFをダウンロードしてPictureBoxに画像として表示する。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ButtonDownload_Click(object sender, EventArgs e)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                /*--- PDF download ---*/
+                // HTTPリクエスト作成
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, REST_API_URL);
+
+                // HTTPヘッダの設定(必要に応じて)
+                //httpRequestMessage.Headers.Add(@"key", value);
+
+                // HTTPリクエスト送信(非同期)
+                HttpResponseMessage httpResponseMessage = null;
+                try
+                {
+                    httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+                }
+                catch (Exception exception)
+                {
+                    // 通信エラー
+                    string message = String.Format("通信エラー : {0}", exception.Message);
+                    Console.WriteLine(exception.StackTrace);
+                    Console.WriteLine(message);
+                    MessageBox.Show(message);
+                    return;
+                }
+                // HTTPステータスチェック
+                if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                {
+                    // 成功
+                    Console.WriteLine("ダウンロード成功");
+                }
+                else
+                {
+                    // 失敗
+                    string message = String.Format("ダウンロードエラー : {0}", httpResponseMessage.ReasonPhrase);
+                    Console.WriteLine(message);
+                    MessageBox.Show(message);
+                    return;
+                }
+                try
+                {
+                    // HTTPレスポンスからPDFデータを取得
+                    using (Stream pdfStream = await httpResponseMessage.Content.ReadAsStreamAsync())
+                    {
+                        // .NETストリームをWindowsランタイムストリームに変換
+                        IInputStream inputStream = WindowsRuntimeStreamExtensions.AsInputStream(pdfStream);
+
+                        // ストリームからPDFオブジェクト作成
+                        PdfDocument pdfDocument = await PdfDocument.LoadFromStreamAsync((IRandomAccessStream)inputStream);
+
+                        // PDFの１ページ目をストリームにレンダリング
+                        InMemoryRandomAccessStream inMemoryRandomAccessStream = new InMemoryRandomAccessStream();
+                        using (PdfPage pdfPage = pdfDocument.GetPage(0))
+                        {
+                            await pdfPage.RenderToStreamAsync(inMemoryRandomAccessStream);
+                        }
+
+                        // ストリームからビットマップオブジェクト作成
+                        Bitmap bitmap = new Bitmap(inMemoryRandomAccessStream.AsStreamForRead());
+
+                        // PixtureBoxにビットマップを描画
+                        pictureBox.Width = bitmap.Width;
+                        pictureBox.Height = bitmap.Height;
+                        pictureBox.Image = bitmap;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    // PDF変換失敗
+                    string message = String.Format("PDFの変換に失敗 : {0}", exception.Message);
+                    Console.WriteLine(exception.StackTrace);
+                    Console.WriteLine(message);
+                    MessageBox.Show(message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 選択した画像ファイルをPDFに変換してMBAASにアップロードする。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonUpload_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
